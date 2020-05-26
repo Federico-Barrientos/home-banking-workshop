c840c6a1a2458444d7b7aae669dd2d8ffbda88bb
Naila Ghigo <nailaghigo20@gmail.com>
2020-05-26 19:08:36 -0300

    agregue boton de volver, modifique las pages en js para que sea mas util

--- js/app.js
+++ js/app.js
@@ -2,21 +2,33 @@
 //Botones
 const btnLogIn = document.getElementById('btn-login');
 const btnSignIn = document.getElementById('btn-signin');
-//Paginas
-const mainPage = document.getElementById('main-page');
-const signIn = document.getElementById('sign-in');
-const logIn = document.getElementById('log-in');
+const btnBack = document.getElementById('btn-back')
 
+//Paginas
+const pages = {
+    main: document.getElementById('main-page'),
+    signIn: document.getElementById('sign-in'),
+    logIn: document.getElementById('log-in')
+}
 
 //Eventos
 btnSignIn.addEventListener('click', (event) => {
     event.preventDefault();
-    mainPage.classList.add('hidden');
-    signIn.classList.remove('hidden');
+    pages.main.classList.add('hidden');
+    pages.signIn.classList.remove('hidden');
 })
 
 btnLogIn.addEventListener('click', (event) => {
     event.preventDefault();
-    mainPage.classList.add('hidden');
-    logIn.classList.remove('hidden');
+    pages.main.classList.add('hidden');
+    pages.logIn.classList.remove('hidden');
 })
+
+function backTo(page) {
+    pages.main.classList.add('hidden');
+    pages.logIn.classList.add('hidden');
+    pages.signIn.classList.add('hidden');
+
+    pages[page].classList.remove('hidden');
+
+}
